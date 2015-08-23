fcc9b16372c1f73cc2636205518e41e8e38f09b7
Wlad Dicario <wlad@2v-prod.com>
2015-08-23 19:38:24 +0200

    Initial Commit

---
+++ WatchOS2-NewUI-Movie-Example WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  WatchOS2-NewUI-Movie-Example WatchKit Extension
+//
+//  Created by Wlad Dicario on 23/08/2015.
+//  Copyright © 2015 Sweefties. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
